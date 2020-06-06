ab9e7ac499bd095ccfb25c56b4570feaadfe3eca
William_ws7 <alexander171296@gmail.com>
2020-06-06 14:35:50 -0400

    definided the routers

--- back/src/index.js
+++ back/src/index.js
@@ -1,12 +1,19 @@
 import express from "express";
 import morgan from "morgan";
+import router from "./routes/task";
+import path from "path";
+
 const app = express();
 
 //Settings
 app.set("port", process.env.PORT || 4000);
 //Middlewares
-
+app.use(morgan("dev"));
+app.use(express.json());
+//static files
+app.use(express.static(path.join(__dirname, "public")));
 //Routers
+app.use("/api/tasks", router);
 app.listen(app.get("port"), () => {
   console.log(`server on port ${app.get("port")}`);
 });
